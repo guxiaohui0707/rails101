88c31e80cddcfb64aa61518af2083cdf3b8857f2
guxiaohui <guxiaohui@duang.sh>
2017-05-09 19:29:44 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
 
   has_many :groups
   has_many :posts
-  has_many :groups
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
 
@@ -16,4 +15,11 @@ class User < ApplicationRecord
         participated_groups.include?(group)
   end
 
+  def join!(group)
+     participated_groups << group
+   end
+
+   def quit!(group)
+     participated_groups.delete(group)
+   end
 end
